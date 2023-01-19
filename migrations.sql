11da3654083bd5f88114bddb6900126b8db07e7e
echnobas <echnobas.git@gmail.com>
2023-01-19 13:32:03 +0000

    change xp command, add migrations

--- migrations.sql
+++ migrations.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE register_group(d_id text)
+CREATE OR REPLACE PROCEDURE REGISTER_GROUP(_d_id text)
 LANGUAGE plpgsql as
 $$
   BEGIN
@@ -7,10 +7,22 @@ $$
   END
 $$;
 
-CREATE OR REPLACE PROCEDURE ADD_XP(d_id text, rbx_id bigint, xp bigint)
+CREATE OR REPLACE PROCEDURE ADD_XP(_d_id text, rbx_id bigint, xp bigint)
 LANGUAGE plpgsql as
 $$
 BEGIN
         EXECUTE 'INSERT INTO ' || quote_ident('data_' || $1) || '.members (rbx_id, d_id, xp) VALUES(' || $2 || ', NULL, ' || $3 || ') ON CONFLICT (rbx_id) DO UPDATE SET xp = members.xp+' || $3 || ' WHERE members.rbx_id = ' || $2 || ';';
+END
+$$;
+
+CREATE OR REPLACE FUNCTION GET_USER(_d_id text, _rbx_id bigint)
+RETURNS SETOF record
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+        RETURN QUERY EXECUTE
+        'SELECT members.rbx_id, members.d_id, members.xp FROM ' || quote_ident('data_' || $1) || '.members
+        WHERE members.rbx_id = ' || $2 ||';';
 END
 $$;
